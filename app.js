d0795f04b34529dc432562e51a6d1942b4b2fef8
Taylor King <kingtb@appstate.edu>
2014-12-31 11:03:38 -0500

    Fixed some express stuff

--- app.js
+++ app.js
@@ -6,21 +6,24 @@ var net = require('net');
 var express = require('express');
 var server = express();
 
-
-
 var host = "127.0.0.1";
 var port = "1701";
 var weight = 10;
-var started = false;
-
+var started = true;
 
 server.get('/start', function(req,res) {
   started = true;
   console.log('start writing');
+  res.send();
+});
+server.get('/weight', function(req,res) {
+  console.log(req);
+  res.send();
 });
 server.get('/stop', function(req, res) {
-  started = fasle;
+  started = false;
   console.log('stop writing');
+  res.send();
 });
 server.listen(3000);
 
@@ -29,17 +32,12 @@ var client = new net.Socket();
 
 // Start the TCP Socket. 
  var socklistener = net.createServer(function(sock) {
-  sock.on('data', function(data) {
-    console.log("Client connected.");
-    while(started) {
-      console.log("Data written: " + data);
-      sock.write(data);
+  while(started) 
+    {
+      sock.write(" 1(  " + weight + "    00\r");
     }
-  });
-  sock.on('close', function(data) {
+   sock.on('close', function(data) {
     console.log("Socket closed");
   });
 });
 socklistener.listen(port, host);
-
-
