e1aa7552c4e0a515455aa697b2adb94f29e6dd81
Jon Johnson <jonjonsonjr@gmail.com>
2015-01-01 22:23:54 -0500

    add package.json and make it work

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
 //Taylor King
 //Under MIT License
-//Simulates a Metler Toledo IND 135 scale reder with TCP
-
+//Simulates a Metler Toledo IND 135 scale reader with TCP
 var net = require('net');
 var express = require('express');
 var server = express();
 
-var host = "127.0.0.1";
-var port = "1701";
+var host = '127.0.0.1';
+var port = '1701';
 var weight = 10;
 var started = true;
 
-server.get('/start', function(req,res) {
+server.get('/start', function (req, res) {
   started = true;
   console.log('start writing');
   res.send();
 });
-server.get('/weight', function(req,res) {
+
+server.get('/weight', function (req, res) {
   weight = req.query.weight;
   res.send();
 });
-server.get('/stop', function(req, res) {
+
+server.get('/stop', function (req, res) {
   started = false;
   console.log('stop writing');
   res.send();
 });
+
 server.listen(3000);
-// Start the TCP Socket. 
-net.createServer(function(sock) {
-  
-  
-  setInterval(function() { 
-    sock.write(" 1(  " + weight + "    00\r");
+
+// Start the TCP Socket.
+net.createServer(function (sock) {
+  setInterval(function () {
+    if (started) {
+      sock.write(' 1(  ' + weight + '    00\r');
+    }
   }, 100);
-    sock.on('close', function(data) {
-    console.log("Socket closed");
+
+  sock.on('close', function (data) {
+    console.log('Socket closed');
   });
 }).listen(port, host);
